38aefca483428be56c0a142a06cd465315e09515
eantank <ankit.antony@ericsson.com>
2022-11-29 13:12:59 +0000

    Added playwright tests for E2E

---
+++ PlaywrightTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.Playwright.MSTest;
+using System.Threading.Tasks;
+
+namespace PlaywrightTests;
+
+[TestClass]
+public class UnitTest1 : PageTest
+{
+    [TestMethod]
+    public async Task ShouldHaveTheCorrectSlogan()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+        await Expect(Page.Locator("text=enables reliable end-to-end testing for modern web apps")).ToBeVisibleAsync();
+    }
+
+    [TestMethod]
+    public async Task ShouldHaveTheCorrectTitle()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+        var title = Page.Locator(".navbar__inner .navbar__title");
+        await Expect(title).ToHaveTextAsync("Playwright");
+    }
+}
