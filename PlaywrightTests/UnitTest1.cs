0d38d116e3edefb21c8fa18d63c3ac84af6dcafe
ankit-antony-devops <116104718+ankit-antony-devops@users.noreply.github.com>
2022-11-29 13:47:00 +0000

    Update UnitTest1.cs

--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -7,17 +7,10 @@ namespace PlaywrightTests;
 public class PageTest : PageTest
 {
     [TestMethod]
-    public async Task ShouldHaveTheCorrectSlogan()
-    {
-        await Page.GotoAsync("https://playwright.dev");
-        await Expect(Page.Locator("text=enables reliable end-to-end testing for modern web apps")).ToBeVisibleAsync();
-    }
-
-    [TestMethod]
     public async Task ShouldHaveTheCorrectTitle()
     {
-        await Page.GotoAsync("https://playwright.dev");
-        var title = Page.Locator(".navbar__inner .navbar__title");
-        await Expect(title).ToHaveTextAsync("Playwright");
+        await Page.GotoAsync("https://bpcalc-csd1.azurewebsites.net");
+        //var title = Page.Locator(".navbar__inner .navbar__title");
+        await Expect(title).ToHaveTextAsync("BP Category Calculator - BPCalculator");
     }
 }
