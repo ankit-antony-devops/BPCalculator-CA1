6b49a99e2682ae20e359079ac88c73b7741ac6ed
eantank <ankit.antony@ericsson.com>
2022-11-29 14:46:27 +0000

    Updated tests

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,20 +2,20 @@
 const { test, expect } = require('@playwright/test');
 
 test('homepage has title and links to intro page', async ({ page }) => {
-  await page.goto('https://bpcalc-csd1.azurewebsites.net/');
+  await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/BP Category Calculator - BPCalculator/);
+  await expect(page).toHaveTitle(/Playwright/);
 
   // create a locator
-  const getStarted = page.getByRole('link', { name: 'Privacy' });
+  const getStarted = page.getByRole('link', { name: 'Get started' });
 
   // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/Privacy');
+  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
 
   // Click the get started link.
   await getStarted.click();
-  
+
   // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*Privacy/);
+  await expect(page).toHaveURL(/.*intro/);
 });
